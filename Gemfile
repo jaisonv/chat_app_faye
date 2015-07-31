e8fe3f7ff5b355f111b45de1d5a6aeb51bbfec14
Jaison Vieira <jaisonnvieira@gmail.com>
2015-07-31 02:08:18 -0300

    Add Ajax

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
+# jQuery plugin for drop-in fix binded events problem caused by Turbolinks
+gem 'jquery-turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
@@ -36,7 +38,8 @@ group :development, :test do
 end
 
 group :production do
-  # Postgre
+  # Postgre SQL
   gem 'pg'
+  # Makes running your Rails app easier. Based on the ideas behind 12factor.net
   # gem 'rails_12factor', '0.0.2'
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,18 +3,24 @@ class CommentsController < ApplicationController
     @comment = Comment.new
     @comments = Comment.order('created_at DESC')
   end
-
+  
   def create
+  respond_to do |format|
     if current_user
       @comment = current_user.comments.build(comment_params)
       if @comment.save
-        flash[:success] = 'Your comment was successfully posted!'
+        flash.now[:success] = 'Your comment was successfully posted!'
       else
-        flash[:error] = 'Your comment cannot be saved.'
+        flash.now[:error] = 'Your comment cannot be saved.'
       end
+      format.html {redirect_to root_url}
+      format.js
+    else
+      format.html {redirect_to root_url}
+      format.js {render nothing: true}
     end
-    redirect_to root_url
   end
+end
 
   private
 
