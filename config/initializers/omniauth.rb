1dbdb57929e70e48d5cff60ae36859e6a5ab166a
Jaison Vieira <jaisonnvieira@gmail.com>
2015-07-31 00:39:53 -0300

    Add task to remove old comments

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,11 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "1863379923887723", "1cd83c110b2248541c73d9c41c6e2ffe",
-           scope: 'public_profile', display: 'page', image_size: 'square'
+if Rails.env.production?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, "1863379923887723", "1cd83c110b2248541c73d9c41c6e2ffe",
+             scope: 'public_profile', display: 'page', image_size: 'square'
+  end
+else
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, "1863794553846260", "0519f7629601357de8fed939221a466f",
+             scope: 'public_profile', display: 'page', image_size: 'square'
+  end
 end
